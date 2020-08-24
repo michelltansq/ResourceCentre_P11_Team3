e571b80a52b5d16efa135f216863344b0c4bd13d
19022187 <19022187@myrp.edu.sg>
2020-08-24 12:42:59 +0800

    updated codes

--- ResourceCentre_P11/src/Chromebook.java
+++ ResourceCentre_P11/src/Chromebook.java
@@ -12,7 +12,8 @@ public class Chromebook extends Item{
 	}
 	
 	public String toString(){
-		String output = "";
+		String cbInfo = super.toString();
+		String output = String.format("%-63s %-20d", cbInfo, os);
 		// Write your codes here
 		return output;
 	}
